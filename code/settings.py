b19dd92165c9e85b56568cf3f48856a99ce9952e
Lucas Bahlmann <lucasbahlmann@Air-von-Lucas.fritz.box>
2025-07-26 14:18:02 +0200

    Initiating git

--- code/settings.py
+++ code/settings.py
@@ -1,7 +1,7 @@
 import pygame, sys
 from pygame.math import Vector2 as vector
 
-WINDOW_WIDTH, WINDOW_HIGHT = 1920, 1080
+WINDOW_WIDTH, WINDOW_HIGHT = 1280, 720
 FPS = 60
 TILE_SIZE = 128
 ANIMATION_SPEED = 10
